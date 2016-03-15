d7f91cfb6fee1f1d862728aa1b70a6ed56267ac3
Gunnar André Reinseth <gunnar.andre.reinseth@nrk.no>
2016-03-15 15:29:00 +0100

    Add example test suites.

---
+++ examples/example1/component1Test.js
@@ -0,0 +1,36 @@
+describe('Component 1', function () {
+  describe('doStuff()', function () {
+    it('does stuff', function () {
+      console.log('do stuff!');
+      console.log('(this should make it to the test report.)');
+    });
+
+    it('pending test');
+
+    describe('nested description 1', function () {
+      beforeEach(function () {
+        console.log('Output in hooks should also make it to the report.');
+      });
+      
+      it('should also do stuff in this case', function () {
+        console.log('Yay!');
+      });
+
+      it('but this fails', function () {
+        throw new Error('No!');
+      });
+    });
+  });
+
+  describe('doOtherStuff()', function () {
+    describe('when stuff is stuffed', function () {
+      it('should be alright', function () {});
+      it('but some things might fail here as well', function () {
+        console.log('Ok, trying to do ...');
+        throw new Error('Boom!');
+      });
+      it('work in progress');
+      it('even more to be done');
+    });
+  });
+});
---
+++ examples/example1/component2Test.js
@@ -0,0 +1,15 @@
+describe('Component 2', function () {
+  it('validates input', function () {});
+  it('fetches data', function () {});
+
+  describe('when data is fetched', function () {
+    it('validates the data', function () {});
+    it('maps the data', function () {});
+    it('returns the data');
+  });
+
+  describe('if the request fails', function () {
+    it('logs the error');
+    it('maps and returns the error');
+  });
+});
